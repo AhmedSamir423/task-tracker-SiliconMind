03e2d6ab0d296e78b50752fb5524de43648cee40
unknown <AhmedSamirElboridy@gmail.com>
2025-07-20 21:49:06 +0300

    Refactor to use migrations for Users and Tasks tables

---
+++ database/migrations/20250720183611-create-users-table.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      user_id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      email: { type: Sequelize.STRING, allowNull: false },
+      password: { type: Sequelize.STRING, allowNull: false }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
